0d2172a0b1cf53f18d6ff2a3bf0550b2ce941b2f
nvpost <nvbpost@yandex.ru>
2021-08-11 17:06:35 +0300

    'activeTags'

--- components/activeTags.php
+++ components/activeTags.php
@@ -1,8 +1,17 @@
 <?php
 deb($filters);
+echo "<div class='active_tags'>";
 foreach ($filters as $key => $f){
     $f = explode(';', $f);
+    echo "<div class='active_tags_line'>";
+    echo "<div class='line_tag_header'>$key: </div>";
     foreach ($f as $i => $tag){
-        deb($tag);
+        echo drowTags($key, $tag);
     }
+    echo "</div>";
+}
+echo "</div>";
+
+function drowTags($key, $tag){
+    return "<div class='active_tags_item'><span>$tag</span><i class='fa fa-times' aria-hidden='true'></i></div>";
 }
