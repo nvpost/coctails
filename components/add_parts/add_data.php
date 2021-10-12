b208808a5838664f624333040d289ce74cf061cc
nvpost <nvbpost@yandex.ru>
2021-10-12 16:54:38 +0300

    absrtactSave

--- components/add_parts/add_data.php
+++ components/add_parts/add_data.php
@@ -1,15 +1,15 @@
 <?php
 
-//$data = json_decode(file_get_contents('php://input'));
+include "../../Classes/SQLModelClass.php";
+require "../../func.php";
 
 $data = $_POST;
 
 
-$file = $_FILES['img'];
-$temp = $file['img']['tmp_name'];
-$file_name = $file['img']['name'];
-$target_file = '../../tmp_img/targetfilename111.jpg';
-move_uploaded_file($_FILES["img"]["tmp_name"], $target_file);
+
+$img_src="";
+
+
 
 $ings = json_decode($data['ing_rows'])->ings;
 array_pop($ings);
@@ -23,9 +23,37 @@ array_pop($process);
 $tags = json_decode($data['tag_list'])->tags;
 
 
+function addToBase(){
+    saveImg();
+    addToSql_coctail(); //coctail_id
+    addToSql_ing();
+    addToSql_tools();
+    addToSql_tags();
+    addToSql_img();
+}
+
+function saveImg(){
+    global $data;
+
+    $file = $_FILES['img'];
+    $file_name = $file['name'];
+    $coctail_img_name = $data['coctail_label_en'];
+    $file_name_arr = explode('.', $file_name);
+    $ext = array_pop($file_name_arr);
+    $img_src = $coctail_img_name.".".$ext;
+    $target_file = "../../tmp_img/{$coctail_img_name}.{$ext}";
+    move_uploaded_file($_FILES["img"]["tmp_name"], $target_file);
+}
+
+
 echo json_encode(['data' => $data,
-    'file'=>$_FILES,
+    'ext'=>$ext,
+    'target_file'=>$target_file,
+    'file'=>$file,
     'ings'=>$ings,
     'tools'=>$tools,
     'process'=>$process,
     'tags'=>$tags]);
+
+
+
